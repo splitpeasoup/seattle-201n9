00d6121d73d69da1de09ed4ee779e229cee201a3
[THXBurke] <[rachelwheels@gmail.com]>
2017-10-10 09:08:58 -0700

    added more lecture code

--- class-06-dom-objects-domain_modeling/lecture-demo/app.js
+++ class-06-dom-objects-domain_modeling/lecture-demo/app.js
@@ -23,3 +23,29 @@ var wolf = {
 //}
 };
 wolf.speak();
+
+///in console to add to the pups array
+//wolf.family.pups.push('Shemp');
+//new funtion to grow the family of the wolf packNum
+
+// wolf.growFamily = function(newName) {
+//   var newPup = {name: newName};
+//   this.family.pups.push(newPup);
+// };
+//
+// wolf.growFamily('Darling');
+
+// Example of THIS:
+// (In console)
+// var shape = {
+// width: 600,
+// height: 400,
+// getArea: function() {
+// 	return this.width * this.height;
+// }
+// };
+//
+// because the this keyword here refers to the shape object, it would be the same as writing:
+//
+// return shape.width * shape.height;
+//
